6b7c1d0df22c54f0fd9066b890e9ff31741c517d
Tomasz Dubiel <dubieltomasz420@gmail.com>
2023-06-05 14:57:10 +0200

    Create Vectors_6.cpp

---
+++ Vectors/Vectors_6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+  
+  std::vector<std::string> grocery = {"Hot Pepper Jam", "Dragon Fruit", "Brussel Sprouts"};
+    
+  // Add more 
+  grocery.push_back("ant milk");
+  grocery.push_back("organic chicken");
+  
+  std::cout << grocery.size();
+}
