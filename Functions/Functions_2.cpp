0e43d243bf973f9105ae377520ad0b2be35c8b49
Tomasz Dubiel <dubieltomasz420@gmail.com>
2023-06-12 12:35:16 +0200

    Create Functions_2.cpp

---
+++ Functions/Functions_2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  // This seeds the random number generator:
+  srand (time(NULL));
+  
+  // Use rand() below to initialize the_amazing_random_number
+  int the_amazing_random_number = rand()%40;
+  std::cout << the_amazing_random_number << std::endl;
+}
