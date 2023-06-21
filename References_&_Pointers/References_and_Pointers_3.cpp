04d001927dff67e0af26a735116ae2404ebd324c
Tomasz Dubiel <dubieltomasz420@gmail.com>
2023-06-21 15:09:42 +0200

    Create References_and_Pointers_3.cpp

---
+++ References_&_Pointers/References_and_Pointers_3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int triple(int &i) {
+
+  i = i * 3;
+  
+  return i;
+
+}
+
+int main() {
+  
+  int num = 1;
+  
+  std::cout << triple(num) << "\n";
+  std::cout << triple(num) << "\n";
+}
